ca21966259dfab43be8847bae80cc02c1c6458ba
VikasKumarSrivastava-web <59119715+VikasKumarSrivastava-web@users.noreply.github.com>
2022-08-25 23:54:34 +0530

    reverse floyd pattern

---
+++ reverseFloyd.py
@@ -0,0 +1,10 @@
+n = int(input("enter the value of row  : "))
+k = 0
+for i in range(n):
+    k=k+i
+m=n+k
+for i in range(n):
+    for j in range(i+1):
+        print(format(m,"<3"),end=" ")
+        m =m-1
+    print()
